dc3de89781642a3364c92737e5f49efa3b552a77
Mat Ruszczycky <mat_ruszczycky@apple.com>
2023-03-13 13:42:00 -0700

    dev: checkpoint

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -62,3 +62,46 @@ document.addEventListener('readystatechange', (e) => {
     App.init();
   }
 });
+
+
+// Import Highway
+import Highway from '@dogstudio/highway';
+
+// Fade
+class Fade extends Highway.Transition {
+  in({ from, to, done }) {
+    // Reset Scroll
+    window.scrollTo(0, 0);
+
+    // Remove Old View
+    from.remove();
+
+    // Animation
+    gsap.fromTo(to, 0.5,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        onComplete: done
+      }
+    );
+  }
+
+  out({ from, done }) {
+    // Animation
+    gsap.fromTo(from, 0.5,
+      { opacity: 1 },
+      {
+        opacity: 0,
+        onComplete: done
+      }
+    );
+  }
+}
+
+
+// Call Highway.Core once.
+const H = new Highway.Core({
+  transitions: {
+    default: Fade
+  }
+});
